59b6f5fc593fa64d0abe452d3a8ceb74b547d3f6
Nguyen Lam <nguyenduclam196@gmail.com>
2020-09-25 10:58:18 +0700

    'nguyenduclam'

--- src/main/java/service/IceCreamDAO.java
+++ src/main/java/service/IceCreamDAO.java
@@ -1,4 +1,54 @@
 package service;
 
-public class IceCreamDAO {
+import model.IceCream;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class IceCreamDAO implements IiceCreamDAO {
+
+    @Override
+    public void insertIceCream(IceCream iceCream) throws SQLException {
+
+    }
+
+    @Override
+    public List<IceCream> selectAllIceCream() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public boolean deleteIceCream(int id) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean updateIceCream(IceCream iceCream) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public IceCream searchIceCream(int id) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public List<IceCream> searchIceCreamByCategory(String idCategory) {
+        return null;
+    }
+
+    @Override
+    public List<IceCream> searchIceCreamByName(String name) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public List<IceCream> sortIceCreamByPrice(String Price) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public List<IceCream> sortIceCreambyQuantity(String Quantity) throws SQLException {
+        return null;
+    }
 }
--- src/main/java/service/IiceCreamDAO.java
+++ src/main/java/service/IiceCreamDAO.java
@@ -2,13 +2,26 @@ package service;
 
 import model.IceCream;
 
+import javax.xml.registry.infomodel.User;
 import java.sql.SQLException;
 import java.util.List;
 
 public interface IiceCreamDAO {
     public void insertIceCream(IceCream iceCream) throws SQLException;
 
-    public IceCream selectIceCream(int id);
+    public List<IceCream> selectAllIceCream() throws SQLException;
 
-    public List<IceCream> selectAllIceCream();
+    public boolean deleteIceCream(int id) throws SQLException;
+
+    public boolean updateIceCream(IceCream iceCream) throws SQLException;
+
+    public IceCream searchIceCream(int id)throws SQLException;
+
+    public List<IceCream> searchIceCreamByCategory(String idCategory);
+
+    public List<IceCream> searchIceCreamByName(String name)throws SQLException;
+
+    public List<IceCream> sortIceCreamByPrice(String Price)throws SQLException;
+
+    public List<IceCream> sortIceCreambyQuantity(String Quantity)throws SQLException;
 }
