75ec341a450a432cb69710da7fec08a17dff43fb
Nguyen Lam <nguyenduclam196@gmail.com>
2020-09-30 14:32:48 +0700

    lam&hung

--- database/database.sql
+++ database/database.sql
@@ -121,7 +121,113 @@ Where ice_cream.ice_cream_id = id;
 END //
 DELIMITER //
 
+//new
+DELIMITER //
+CREATE PROCEDURE select_all_ice_by_status()
+BEGIN
+SELECT * FROM ice_cream
+where ice_cream.status = 0;
+END //
+DELIMITER //
 
+//new
+DELIMITER //
+CREATE procedure delete_ice_status(in id int)
+BEGIN
+update ice_cream
+set ice_cream.status = 1
+where ice_cream.ice_cream_id=id;
+END //
+DELIMITER //
+
+SELECT * FROM ice_cream_shop.users;
+
+
+DELIMITER $$
+CREATE procedure search_User(in name varchar(100))
+BEGIN
+SELECT users.user_id, users.user_name, users.user_password, users.user_email
+FROM users
+WHERE users.user_name = name;
+END $$
+DELIMITER $$
+
+Call search_User('nguyenduclam196')
+
+DELIMITER $$
+CREATE PROCEDURE insert_item(order_id int, ice_cream_id int, pice_discount double, quality int, total_money double)
+BEGIN
+    INSERT INTO item(order_id,ice_cream_id,pice_discount,quality,total_money)
+    VALUES(order_id,ice_cream_id,pice_discount,quality,total_money);
+END $$
+DELIMITER $$
+
+call insert_item(11, 1,10,2,0);
+
+DELIMITER //
+CREATE procedure search_item_by_id_order(in id_order int)
+BEGIN
+SELECT item.item_id,item.order_id,item.ice_cream_id,item.pice_discount,item.quality,item.total_money
+FROM item
+Where item.order_id = id_order;
+END //
+DELIMITER //
+
+
+DELIMITER //
+CREATE procedure select_all_item()
+BEGIN
+SELECT *
+FROM item;
+END //
+DELIMITER //
+
+
+
+DELIMITER //
+CREATE procedure sum_money_item_by_id_order(in id_order int)
+BEGIN
+SELECT sum(item.quality*ice_cream.price)
+FROM item
+join ice_cream on item.ice_cream_id = ice_cream.ice_cream_id
+Where item.order_id = id_order
+group by item.order_id;
+END //
+DELIMITER //
+
+call sum_money_item_by_id_order(15)
+
+DELIMITER $$
+CREATE PROCEDURE insert_order(name_customer varchar(50), address_customer varchar(200), numberphone_customer varchar(20), total_money double(10,3) )
+BEGIN
+    INSERT INTO orders(name_customer , address_customer , numberphone_customer , total_money )
+    VALUES(name_customer,address_customer,numberphone_customer,total_money);
+END $$
+DELIMITER $$
+
+
+DELIMITER $$
+CREATE PROCEDURE select_id_last()
+BEGIN
+    select  orders.order_id
+    From orders
+    order by orders.order_id DESC
+    LiMit 1;
+END $$
+DELIMITER $$
+
+
+DELIMITER $$
+CREATE PROCEDURE select_all()
+BEGIN
+   select * from orders;
+END $$
+DELIMITER $$
+
+call insert_order('lam', 'hanoi', 0123,0);
+
+alter table ice_cream
+add status int DEFAULT 0;
 
  
  
